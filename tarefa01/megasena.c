f90ffe7596e0d8ce3f0b5869bde92c2c43678df5
ra195030 <viniluizfarias@gmail.com>
2020-09-22 17:58:16 -0700

    Foi finalizada a tarefa 01

--- tarefa01/megasena.c
+++ tarefa01/megasena.c
@@ -1,51 +1,52 @@
 #include <stdio.h>
 
+void receber_entrada(int quant_apostas,int apostas[][60],int nums_sorteados[]){
+    int i,j;
+    for (i = 0;i<quant_apostas;i++)
+        for(j = 0;j<60;j++)
+            scanf(" %d",&apostas[i][j]);
+
+    for (i = 0;i<6;i++)
+        scanf("%d",&nums_sorteados[i]);
+}
+
 int contar_acertos(int i,int nums_sorteados[6],int apostas[][60]){
     int j;
     int contador = 0;
-    for(j = 0;j<6;j++){
+    for(j = 0;j<6;j++)
         if (apostas[i][nums_sorteados[j]-1] == 1){
             contador ++;
         }
-    }
     return contador;
 }
 
-void anunciar_premiados(double premio,int pedacos_do_premio[6],int quant_acertos[1000],int quant_de_apostas_por_acerto[6],int quant_apostas){
-    int i;
+void anunciar_premiados(double premio,int pedacos_do_premio[6],int quant_acertos[1000],int quant_de_apostas_por_acertos[6],int quant_apostas){
+    int i,pedaco_do_premio;
     for (i = 0; i <quant_apostas;i++){
-        printf("%.2lf\n",premio*pedacos_do_premio[quant_acertos[i]-1]/100/quant_de_apostas_por_acerto[quant_acertos[i]-1]);
+        pedaco_do_premio = pedacos_do_premio[quant_acertos[i]];
+        printf("%.2lf\n",premio*pedaco_do_premio/100/quant_de_apostas_por_acertos[quant_acertos[i]]);
     }
 }
 
 
 int main(){
-    int quant_apostas,apostas[1000][60],nums_sorteados[6],quant_acertos[1000];
-    int i,j;
-    double premio;
+    int quant_apostas=0,apostas[1000][60],nums_sorteados[6],quant_acertos[1000];
+    int i;
+    double premio=0;
 
     scanf(" %d %lf",&quant_apostas,&premio);
-    for (i = 0;i<quant_apostas;i++){
-        for(j = 0;j<60;j++){
-            scanf(" %d",&apostas[i][j]);
-        }
-    }
-    for (i = 0;i<6;i++){
-        scanf("%d",&nums_sorteados[i]);
-    }
+    receber_entrada(quant_apostas,apostas,nums_sorteados);
 
-
-    for (i = 0;i<quant_apostas;i++){
+    for (i = 0;i<quant_apostas;i++)
         quant_acertos[i] = contar_acertos(i,nums_sorteados,apostas);
-    }
 
-    int quant_de_apostas_por_acerto[6];
+    int quant_de_apostas_por_acertos[7];
 
-    for (i = 0; i <quant_apostas;i++){
-        quant_de_apostas_por_acerto[quant_acertos[i]-1]++;
-    }
-    int pedacos_do_premio[6] = {0,0,0,19,19,62};
+    for (i = 0; i <quant_apostas;i++)
+        quant_de_apostas_por_acertos[quant_acertos[i]]++;
 
-    anunciar_premiados(premio,pedacos_do_premio,quant_acertos,quant_de_apostas_por_acerto,quant_apostas);
+    int pedacos_do_premio[7] = {0,0,0,0,19,19,62};
+
+    anunciar_premiados(premio,pedacos_do_premio,quant_acertos,quant_de_apostas_por_acertos,quant_apostas);
         
 }
