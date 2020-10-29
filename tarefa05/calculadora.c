d1c8d9ba04c9c1255a5e58a9b1e5f5a3a9c6573c
ra195030 <v195030@dac.unicamp.br>
2020-10-29 16:55:47 -0400

    foi feita parte da tarefa 05

--- tarefa05/calculadora.c
+++ tarefa05/calculadora.c
@@ -3,25 +3,120 @@
 #define MAX 25;
 
 typedef struct No{
-    int numero;
+    int digito;
+    struct No *ant;
     struct No *prox;
 } No;
 
 typedef struct No * p_no;
 
+typedef struct Numero_grande{
+    int comprimento;
+    p_no digitos;
+}
+
+void printar_numero(Numero_grande numero){
+    p_no digito_atual = (numero.digitos)->ant;
+    for(int i = 0,i<numero.comprimento,i++){
+        printf("%d",digito_atual->digito);
+        digito_atual = digito_atual->ant;
+    }
+    printf("\n");
+}
+
 p_no criar_lista(){
     return NULL;
 }
 
-p_no adicionar_na_lista(No *lista,char digito){
+p_no adicionar_na_lista(p_no lista,char digito,p_no inicial){
     p_no novo;
     novo = malloc(sizeof(No));
-    novo->prox=lista;
-    novo->numero = (int)digito;
-    return novo;
+    if (lista == NULL){
+        novo->prox = novo;
+        novo->ant = novo;
+        lista = novo;
+    }else{
+        novo->prox= lista->prox;
+        novo->ant = lista;
+        lista->prox = novo;
+        novo->digito = (int)digito;
+    }
+    return lista;
 }
 
-void chamar_operacao(char operacao,No *num1,No *num2){
+void somar(Numero_grande num1,Numero_grande num2){
+    p_no resultado;
+    p_no inicial1 = num1.digitos,inicial2 = num2.digitos;
+    p_no digito_atual1 = inicial1,digito_atual2 = inicial2
+    
+    
+    int soma_atual = 0;
+    int acabou1= 0, acabou2= 0;
+
+    while(acabou1 == 0 && acabou2 == 0){
+        if(acabou1 == 0){
+            soma_atual += digito_atual1->digito;
+            digito_atual1 = digito_atual1->prox;
+            if (digito_atual1 == inicial1){
+                acabou1 =1;
+            }
+        }
+        if(acabou2 == 0){
+            soma_atual += digito_atual2->digito;
+            digito_atual2 = digito_atual2->prox;
+            if (digito_atual2 == inicial2){
+                acabou2 =1;
+            }
+        }
+        adicionar_na_lista(resultado,soma_atual);
+        soma_atual = soma_atual/10;
+    }
+}
+
+
+void subtrair(Numero_grande num1,Numero_grande num2){
+    if (num1.comprimento < num2.comprimento){
+        subtrair(num2,num1);
+        return;
+    }
+    if (num1.comprimento == num2.comprimento && num1->digitos->digito < num2->digitos->digito){
+        subtrair(num2,num1);
+        return;
+    }
+    p_no resultado = criar_lista();
+    p_no inicial1 = num1.digitos,inicial2 = num2.digitos;
+    p_no digito_atual1 = num1.digitos,digito_atual2 = num2.digitos;
+
+    int subtracao_atual = 0;
+    int acabou1= 0, acabou2= 0;
+    while(acabou1 == 0 && acabou2 == 0){
+
+        subtracao_atual += digito_atual1->digito;
+        digito_atual1 = digito_atual1->prox;
+        if (digito_atual1 == inicial1){
+            acabou1 =1;
+        }
+        
+        if(acabou2 == 0){
+            if(digito_atual2->digito > subtracao_atual){
+                subtracao_atual += 10;
+                digito_atual1->digito -= 1; 
+            }
+            subtracao_atual -= digito_atual2->digito;
+            digito_atual2 = digito_atual2->prox;
+            if (digito_atual2 == inicial2){
+                acabou2 =1;
+            }
+        }
+        adicionar_na_lista(resultado,subtracao_atual);
+    }
+    printf("\n");
+}
+
+
+}
+
+void chamar_operacao(char operacao,Numero_grande num1,Numero_grande num2){
     if (operacao == '+'){
         somar(num1,num2);
     }else if(operacao == '-'){
@@ -35,13 +130,17 @@ void chamar_operacao(char operacao,No *num1,No *num2){
 
 void armazenar_numero(p_no num1){
     char digito;
+    inicial = num1;
+
     for(int j = 0;j<MAX;j++){
         scanf("%c",&digito);
         if (digito == ' '){
             break;
         }
-        num1 = adicionar_na_lista(num1,digito);
+        num1 = adicionar_na_lista(num1,digito,inicial);
     }
+
+
 }
 
 
@@ -53,11 +152,14 @@ int main(){
         char operacao;
         scanf("%c",&operacao);
 
-        p_no num1= criar_lista(),p_no num2= criar_lista();
-        armazenar_numero(num1);
-        armazenar_numero(num2);
+        NUMERO num1 , num2;
+        num1.digitos = criar_lista();
+        num2.digitos = criar_lista();
 
-        chamar_operacao(operacao,num1,num2)
+        armazenar_numero(num1.digitos);
+        armazenar_numero(num2.digitos);
+
+        chamar_operacao(operacao,num1,num2);
 
     }
 }
