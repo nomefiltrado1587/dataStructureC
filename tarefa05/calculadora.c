8a56c800b1d10535ecd1ec8cb460e221e4be2a58
ra195030 <v195030@dac.unicamp.br>
2020-10-31 18:32:37 -0400

    foi implementada a soma na tarefa 05

--- tarefa05/calculadora.c
+++ tarefa05/calculadora.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define MAX 25;
+#define MAX 25
 
 typedef struct No{
-    int digito;
-    struct No *ant;
+    int dado;
     struct No *prox;
+    struct No *ant;
 } No;
 
 typedef struct No * p_no;
@@ -13,153 +13,203 @@ typedef struct No * p_no;
 typedef struct Numero_grande{
     int comprimento;
     p_no digitos;
-}
+} Numero_grande;
 
-void printar_numero(Numero_grande numero){
-    p_no digito_atual = (numero.digitos)->ant;
-    for(int i = 0,i<numero.comprimento,i++){
-        printf("%d",digito_atual->digito);
-        digito_atual = digito_atual->ant;
-    }
-    printf("\n");
-}
-
-p_no criar_lista(){
-    return NULL;
-}
-
-p_no adicionar_na_lista(p_no lista,char digito,p_no inicial){
+p_no adicionar_elemento(p_no lista,int dado){
     p_no novo;
     novo = malloc(sizeof(No));
-    if (lista == NULL){
+    novo->dado = dado;
+    if (lista == NULL) {
         novo->prox = novo;
         novo->ant = novo;
         lista = novo;
-    }else{
-        novo->prox= lista->prox;
+    } else {
+
         novo->ant = lista;
+        lista->prox->ant = novo;
+        novo->prox = lista->prox;
         lista->prox = novo;
-        novo->digito = (int)digito;
     }
     return lista;
 }
 
-void somar(Numero_grande num1,Numero_grande num2){
-    p_no resultado;
-    p_no inicial1 = num1.digitos,inicial2 = num2.digitos;
-    p_no digito_atual1 = inicial1,digito_atual2 = inicial2
-    
-    
-    int soma_atual = 0;
-    int acabou1= 0, acabou2= 0;
-
-    while(acabou1 == 0 && acabou2 == 0){
-        if(acabou1 == 0){
-            soma_atual += digito_atual1->digito;
-            digito_atual1 = digito_atual1->prox;
-            if (digito_atual1 == inicial1){
-                acabou1 =1;
-            }
-        }
-        if(acabou2 == 0){
-            soma_atual += digito_atual2->digito;
-            digito_atual2 = digito_atual2->prox;
-            if (digito_atual2 == inicial2){
-                acabou2 =1;
-            }
-        }
-        adicionar_na_lista(resultado,soma_atual);
-        soma_atual = soma_atual/10;
+void armazenar_numero(char *numero,Numero_grande *num){
+    int dado;
+    for(int i = 0;numero[i] != '\0';i++){
+        num->comprimento +=1;
+        dado = numero[i]-48;
+        num->digitos = adicionar_elemento(num->digitos,dado);
     }
 }
 
-
-void subtrair(Numero_grande num1,Numero_grande num2){
-    if (num1.comprimento < num2.comprimento){
-        subtrair(num2,num1);
-        return;
+void destruir_lista(p_no lista){
+    p_no inicial = lista;
+    p_no prox;
+    while(lista->prox != inicial){
+        prox = lista->prox;
+        free(lista);
+        lista = prox;
     }
-    if (num1.comprimento == num2.comprimento && num1->digitos->digito < num2->digitos->digito){
-        subtrair(num2,num1);
-        return;
-    }
-    p_no resultado = criar_lista();
-    p_no inicial1 = num1.digitos,inicial2 = num2.digitos;
-    p_no digito_atual1 = num1.digitos,digito_atual2 = num2.digitos;
+    free(lista);
+}
 
-    int subtracao_atual = 0;
-    int acabou1= 0, acabou2= 0;
-    while(acabou1 == 0 && acabou2 == 0){
-
-        subtracao_atual += digito_atual1->digito;
-        digito_atual1 = digito_atual1->prox;
-        if (digito_atual1 == inicial1){
-            acabou1 =1;
-        }
-        
-        if(acabou2 == 0){
-            if(digito_atual2->digito > subtracao_atual){
-                subtracao_atual += 10;
-                digito_atual1->digito -= 1; 
-            }
-            subtracao_atual -= digito_atual2->digito;
-            digito_atual2 = digito_atual2->prox;
-            if (digito_atual2 == inicial2){
-                acabou2 =1;
-            }
-        }
-        adicionar_na_lista(resultado,subtracao_atual);
-    }
+void imprimir_lista(p_no lista) {
+    p_no inicial = lista;
+    do{
+        printf("%d",lista->dado);
+        lista = lista->ant;
+    }while(lista != inicial);
     printf("\n");
 }
 
+void imprimir_lista_invertida(p_no lista) {
+    p_no inicial = lista;
+    do{
+        lista = lista->prox;
+        printf("%d",lista->dado);
+    }while(lista != inicial);
+    printf("\n");
+}
+
+p_no remover_da_lista(p_no no){
+    p_no prox = no->prox;
+    (no->prox)->ant = no->ant;
+    (no->ant)->prox = no->prox;
+    free(no);
+    return prox;
+}
+
+void receber_numero(Numero_grande *num){
+    char numero[25];
+    num->digitos = NULL;
+    num->comprimento = 0;
+    scanf(" %s",numero);
+    armazenar_numero(numero,num);
+}
+
+p_no somar(Numero_grande num1,Numero_grande num2){
+    if(num1.comprimento < num2.comprimento){
+        return somar(num2,num1);
+    }
+    p_no resultado = NULL,inicial1 = num1.digitos->prox,inicial2 = num2.digitos->prox;
+    p_no digito_atual1 = num1.digitos->prox,digito_atual2 = num2.digitos->prox;
+    int soma_atual;
+    int acabou1=0,acabou2 = 0;
+    resultado = adicionar_elemento(resultado,0);
+    while(acabou1 == 0){
+        soma_atual = 0;
+        resultado = adicionar_elemento(resultado,0);
+
+        soma_atual  += digito_atual1->dado;
+        if(digito_atual1->prox == inicial1){
+            acabou1 = 1;
+        }else{
+            digito_atual1 = digito_atual1->prox;
+        }
+        if (acabou2 == 0){
+            soma_atual  += digito_atual2->dado;
+            if(digito_atual2->prox == inicial2){
+                acabou2 = 1;
+            }else{
+                digito_atual2 = digito_atual2->prox;
+            }
+        }
+        resultado->prox->dado = (soma_atual+resultado->dado)%10;
+        resultado->dado = (soma_atual+resultado->dado)/10;
+    }
+    if(resultado->dado == 0){
+        resultado = remover_da_lista(resultado);
+    }
+    return resultado->ant;
+}
+
+p_no subtrair(Numero_grande num1,Numero_grande num2){
+    if(num1.comprimento < num2.comprimento){
+        return subtrair(num2,num1);
+    }
+    if(num1.comprimento == num2.comprimento){
+        p_no digito1= num1.digitos,digito2= num2.digitos;
+        for(int i =0;i<num1.comprimento;i++){
+            if(digito1->dado < digito2->dado){
+                return subtrair(num2,num1);
+            }
+            digito1 = digito1->prox;
+            digito2 = digito2->prox;
+        }
+    }
+
+    p_no resultado = NULL,inicial1 = num1.digitos->prox,inicial2 = num2.digitos->prox;
+    p_no digito_atual1 = num1.digitos->prox,digito_atual2 = num2.digitos->prox;
+    int subtracao_atual=0;
+    int acabou1=0,acabou2 = 0;
+
+    while(acabou1 == 0){
+        resultado = adicionar_elemento(resultado,0);
+
+        subtracao_atual += digito_atual1->dado;
+        if (digito_atual1->prox == inicial1){
+            acabou1 = 1;
+        }else{
+            digito_atual1 = digito_atual1->prox;
+        }
+
+        if (acabou2 == 0){
+            if(digito_atual2->dado==subtracao_atual){
+                resultado->dado = (subtracao_atual + 10 - digito_atual2->dado);
+                subtracao_atual = -1;
+            }else{
+                resultado->dado = subtracao_atual - digito_atual2->dado;
+                subtracao_atual = 0;
+            }
+            if (digito_atual2->prox == inicial2){
+                acabou2 = 1;
+            }else{
+                digito_atual2 = digito_atual2->prox;
+            }
+        }else{
+            resultado->dado = subtracao_atual;
+        }
+
+    }
+    return resultado;
+
 
 }
 
-void chamar_operacao(char operacao,Numero_grande num1,Numero_grande num2){
-    if (operacao == '+'){
-        somar(num1,num2);
-    }else if(operacao == '-'){
-        subtrair(num1,num2);
-    }else if(operacao == '*'){
+void realizar_operacao(){
+    char tipo_operacao;
+    Numero_grande num1,num2;
+    scanf(" %c",&tipo_operacao);
+    receber_numero(&num1);
+    receber_numero(&num2);
+
+    p_no resultado;
+    if(tipo_operacao == '+'){
+        resultado = somar(num1,num2);
+        imprimir_lista_invertida(resultado);
+    }
+    if(tipo_operacao == '-'){
+        resultado =  subtrair(num1,num2);
+        imprimir_lista_invertida(resultado);
+    }
+    /*
+    if(tipo_operacao == '*'){
         multiplicar(num1,num2);
-    }else{
+    }
+    if(tipo_operacao == '/'){
         dividir(num1,num2);
     }
+    */
+    destruir_lista(num1.digitos);
+    destruir_lista(num2.digitos);
+    destruir_lista(resultado);
 }
-
-void armazenar_numero(p_no num1){
-    char digito;
-    inicial = num1;
-
-    for(int j = 0;j<MAX;j++){
-        scanf("%c",&digito);
-        if (digito == ' '){
-            break;
-        }
-        num1 = adicionar_na_lista(num1,digito,inicial);
-    }
-
-
-}
-
 
 int main(){
     int n;
     scanf("%d",&n);
+
     for (int i = 0;i<n;i++){
-
-        char operacao;
-        scanf("%c",&operacao);
-
-        NUMERO num1 , num2;
-        num1.digitos = criar_lista();
-        num2.digitos = criar_lista();
-
-        armazenar_numero(num1.digitos);
-        armazenar_numero(num2.digitos);
-
-        chamar_operacao(operacao,num1,num2);
-
+        realizar_operacao();
     }
 }
