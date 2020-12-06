84ca009930b28e1b57f84197be29e7916c9b278b
ra195030 <v195030@dac.unicamp.br>
2020-12-06 02:20:48 -0500

    Foi finalizada a tarefa 10

--- tarefa10/corretor.c
+++ tarefa10/corretor.c
@@ -131,7 +131,7 @@ int procurar_similares(char palavra[MAX_PALAVRA],Hash *dicionario){
     //procura palavras com uma letra a menos do que a palavra que foi inserida
     for(int j = 0;j<MAX_PALAVRA && achou != 1;j++){
         copiar_palavra(palavra,palavra_aux);
-        for(int k = j;palavra[k]!='\0';k++){
+        for(int k = j;palavra[k+1]!='\0';k++){
             palavra_aux[k]=palavra[k+1];
         }
         achou = procurar_no_hash(palavra_aux,dicionario);
