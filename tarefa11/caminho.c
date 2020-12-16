642262508b2b0fa2f3d455499c79ecf5d03d5218
ra195030 <v195030@dac.unicamp.br>
2020-12-16 13:19:44 -0500

    Foi finalizada a tarefa 11

--- tarefa11/caminho.c
+++ tarefa11/caminho.c
@@ -18,6 +18,7 @@ typedef struct Grafo{
 typedef Grafo * p_grafo;
 
 Grafo inicializar_grafo(){
+    // INICIALIZA UM GRAFO
     Grafo grafo;
     grafo.n = 0;
     grafo.matriz_distancias = NULL;
@@ -26,12 +27,14 @@ Grafo inicializar_grafo(){
 }
 
 void zerar_vetor(int *v,int n){
+    // TRANSFORMA UM VETOR QUALQUER EM UM VETOR NULO
     for (int i = 0;i<n;i++){
         v[i] = 0;
     }
 }
 
 void receber_ponto_inicial(p_vertice inicio){
+    // RECEBE O PONTO INICIAL E O ARMAZENA EM UM VERTICE
     double x_inicial,y_inicial;
     scanf(" %lf %lf", &x_inicial,&y_inicial);
     inicio->x = x_inicial;
@@ -40,7 +43,7 @@ void receber_ponto_inicial(p_vertice inicio){
 }
 
 p_vertice adicionar_vertice(p_vertice lista,double x, double y){
-
+    // ADICIONA UM VERTICE EM UMA LISTA LIGADA
     p_vertice novo = malloc(sizeof(Vertice));
     novo->x = x;
     novo->y = y;
@@ -50,6 +53,7 @@ p_vertice adicionar_vertice(p_vertice lista,double x, double y){
 }
 
 void liberar_grafo(p_grafo grafo){
+    // LIBERA UM GRAFO
     for(int i = 0;i<grafo->n;i++){
         free(grafo->vertices[i]);
         free(grafo->matriz_distancias[i]);
@@ -60,6 +64,7 @@ void liberar_grafo(p_grafo grafo){
 }
 
 int distancia_inteira(p_vertice no1,p_vertice no2){
+    // RETORNA A MENOR DISTANCIA INTEIRA MAIOR DO QUE A DISTANCIA ENTRE DOIS VERTICES
     double distancia = 0;
     distancia += (no1->x-no2->x)*(no1->x-no2->x);
     distancia += (no1->y-no2->y)*(no1->y-no2->y);
@@ -69,6 +74,7 @@ int distancia_inteira(p_vertice no1,p_vertice no2){
 }
 
 void armazenar_vertices_no_grafo(p_grafo grafo, p_vertice lista_de_vertices,int quantidade_de_vertices){
+    // COLOCA OS VALORES ARMAZENADOS NA LISTA LIGADA DE VERTICES NO VETOR DE p_vertices DO GRAFO
     grafo->n = quantidade_de_vertices;
     grafo->vertices = malloc(sizeof(p_vertice)*grafo->n);
     for(int i = 0;i<grafo->n;i++){
@@ -78,6 +84,7 @@ void armazenar_vertices_no_grafo(p_grafo grafo, p_vertice lista_de_vertices,int 
 }
 
 void criar_matriz_de_distancias(p_grafo grafo,int *aresta_min,int *aresta_max,int *aresta_atual){
+    // CALCULA A DISTANCIA ENTRE CADA UM DOS VERTICES E OS ARMAZENA NA MATRIZ DO GRAFO
     int i,j,n,distancia_atual;
     n = grafo->n;
 
@@ -105,6 +112,8 @@ void criar_matriz_de_distancias(p_grafo grafo,int *aresta_min,int *aresta_max,in
 }
 
 void receber_entrada(p_vertice *lista_de_vertices,p_vertice *lista_de_lugias,int *quantidade_de_vertices,int *quantidade_de_lugias){
+    //ARMAZENA OS POKESTOPS EM lista_de_vertices, E OS LUGIAS EM lista_de_lugias, AMBOS SAO LISTAS LIGADAS
+    // EM SEGUIDA JUNTA AS DUAS LISTAS. ESSE PROCESSO É FEITO PARA QUE NO GRAFO OS PRIMEIROS VERTICES SEJAM LUGIAS
     double x_atual,y_atual;
     char tipo[15];
 
@@ -127,6 +136,8 @@ void receber_entrada(p_vertice *lista_de_vertices,p_vertice *lista_de_lugias,int
 }
 
 void calcular_nova_aresta(int *aresta_atual, int *aresta_min,int *aresta_max,int *tem_caminho,int *acabou){
+    // CASO ALGUM CAMINHO TENHA SIDO ENCONTRADO PARA A INTERAÇÃO ANTERIRIOR A NOVA ARESTA SERÁ A MEDIA ENTRE A ATUAL E MINIMA
+    // CASO NAO TENHA SIDO ENCONTRADO SERA  A MEDIA ENTRA  A MAXIMA E A ATUAL
     int aux;
     if(*tem_caminho == 1){
         aux = *aresta_atual;
@@ -147,6 +158,8 @@ void calcular_nova_aresta(int *aresta_atual, int *aresta_min,int *aresta_max,int
 }
 
 int chega_em_um_lugia(p_grafo g,int quantidade_de_lugias,int distancia_maxima,int v){
+    // CONFERE SE DETERMINADO VÉRTICE ESTA A UMA DISTANCIA MENOR OU IGUAL QUE distancia_maxima DE UM DOS LUGIAS
+    // RETORNANDO 1 PARA SIM E 0 PARA NAO
     for(int i = 0;i<quantidade_de_lugias;i++){
         if(g->matriz_distancias[i][v]<= distancia_maxima){
             return 1;
@@ -156,6 +169,7 @@ int chega_em_um_lugia(p_grafo g,int quantidade_de_lugias,int distancia_maxima,in
 }
 
 int busca_rec(p_grafo g, int *visitado , int v,int quantidade_de_lugias,int distancia_maxima) {
+    // CONFERE RECURSIVAMENTE SE UM VERTICE ESTA A UMA DISTANCIA MENOR QUE distancia_maxima DE UM LUGIA
     int w;
     if (chega_em_um_lugia(g,quantidade_de_lugias,distancia_maxima,v)){
         return 1;
@@ -171,7 +185,9 @@ int busca_rec(p_grafo g, int *visitado , int v,int quantidade_de_lugias,int dist
     return 0;
 }
 
-int calcular_distancia(p_grafo grafo,p_vertice inicio,int *aresta_atual,int *aresta_max,int *aresta_min,int quantidade_de_lugias){
+int existe_caminho(p_grafo grafo,p_vertice inicio,int *aresta_atual,int *aresta_max,int *aresta_min,int quantidade_de_lugias){
+    // CONFERE SE EXISTE UM CAMINHO POSSIVEL ENTRE O INICIO E ALGUM LUGIA COM DISTANCIA MAXIMA
+    // ENTRE VERTICES IGUAL A aresta_atual, DE FORMA "BINARIA" (log(n))
     int *ja_foram = malloc(grafo->n * sizeof(int));
     int acabou = 0,tem_caminho = 0;
 
@@ -207,8 +223,10 @@ int main(){
 
     armazenar_vertices_no_grafo(&grafo,lista_de_lugias,quantidade_de_vertices);
     criar_matriz_de_distancias(&grafo,&aresta_min,&aresta_max,&aresta_atual);
-    int acabou = calcular_distancia(&grafo,&inicio,&aresta_atual,&aresta_max,&aresta_min,quantidade_de_lugias);
 
+    int acabou = existe_caminho(&grafo,&inicio,&aresta_atual,&aresta_max,&aresta_min,quantidade_de_lugias);
+    // acabou = 1 -> A ULTIMA ARESTA TESTADA É A MENOR POSSIVEL (o ultimo teste deu certo)
+    // acabou = 2 -> A ULTIMA ARESTA TESTADA É 1 A MENOS DO QUE A MENOR POSSÍVEL (o ultimo teste deu errado)
     liberar_grafo(&grafo);
     if (acabou == 1){
         printf("%d",aresta_atual);
